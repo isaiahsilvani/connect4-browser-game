8e0cc0567a9538ec0ea485df4786f6c8becad031
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-05 12:09:22 -0500

    begin adding functionality

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const colors = {
     '-1': 'yellow'
 }
 
+
 /*-------------------------------- Variables --------------------------------*/
 let board;
 let turn;
@@ -27,29 +28,35 @@ squares.forEach((square) => {
 
 /*-------------------------------- Functions --------------------------------*/
 function handleClick(evt) {
+    let ele = evt.target
+    if (ele.className === 'blank') {
     // IF THE SPACE CURRENT PLAYER CLICKS ON IS BLANK, set first number in  ID to 0 so we can start at the top. Then, CHECK THE BOTTOM SPACE IN BOARD TO SEE IF ITS EMPTY. IF IT IS, CHECK THE NEXT BOTTOM SPACE. WHEN NEXT BOTTOM IS FILLED, OR YOU HAVE REACHED THE BOTTOM, FILL BACKGROUND SPACE ACCORDING TO TURN (1 or -1)
-    let position = evt.target.getAttribute('id')
-    // SET THE POSITION TO THE TOP, KEEP COLUMN THE SAME. SEPERATE ROW AND COLUMN IN ELE'S ID
-    let row = 0
-    let column = position[1]
-    console.log(row + column)
-    //nextEmptySpace
+    ele.className = 'player1'
+        let position = ele.getAttribute('id')
+        // SET THE POSITION TO THE TOP, KEEP COLUMN THE SAME. SEPERATE ROW AND COLUMN IN ELE'S ID
+        position = Number(position)
+        //get remainder of 7 to start position at the top of the board
+        position = position % 7
+        // if the top row of current column is null, increase position by 7. If new position on board is null, increasse by 7. If new position of board is null, increase by 7. If new position of board is filled with something other than null, go back 7 and change that element's class
+        let test = lastEmptySpaceInColumn(position)
+        console.log('output', test)
+    }
 }
 
-// function nextEmptySpace(row, column) {
-//     // make a for loop. If the next row "beneath" in the same column is null, continue the loop. If the row beneath is not null, return the row and column position that is empty
-//     for (i = 0; i < 7; i++) {
-//         row++
-//         let idx = row + column;
-//         if (board[idx] === null) {
-//             return true;
-//         }
-//     }
-// }
-
-
-
-
+function lastEmptySpaceInColumn(position) {
+    console.log('position', position)
+    let newPosition = position + 7;
+    if (board[newPosition] === null) {
+        lastEmptySpaceInColumn(newPosition)
+    } else if (board[newPosition] !== null) {
+        console.log('finalPosition', newPosition)
+        //THIS IS WHERE THE PROBLEM IS!!
+        console.log(newPosition - 7)
+        console.log('for the love of god return poop please')
+        return "poop"
+    }
+}
+// return the number of the last empty space of a connect4 board, by checking to see if it's null. If we get a position >= 35, we have identifed that we are at the bottom of the board and it is time to break the loop and set the corresponding div id element with a player1 or player2 color
 function init() {
     //SET AN ARRAY TO REPRESENT THE GAME BOARD, PLAYER 1 = TURN WINNER IS NULL
     board = [
@@ -59,6 +66,7 @@ function init() {
         null, null, null, null, null, null, null, 
         null, null, null, null, null, null, null, 
         null, null, null, null, null, null, null, 
+        'btm', 'btm', 'btm', 'btm', 'btm', 'btm', 'btm', 
     ];
     turn = 1
     winner = null
