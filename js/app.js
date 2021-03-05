24d946549b1dc1edf9b2ebd12fd50ffe3e4e6ab5
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-05 09:32:28 -0500

    connect html elements to js with dom

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@
 
 /*------------------------ Cached Element References ------------------------*/
 const player1nameInp = document.getElementById('player1-name')
-const player2nameInp = document.getElementById('player1-name')
-const squares = document.querySelectorAll('.blank')
-
+const player1nameBtn = document.getElementById('player1-btn')
+const player2nameInp = document.getElementById('player2-name')
+const player2nameBtn = document.getElementById('player2-btn')
+const squares = document.querySelectorAll('#board-grid > div')
+const message = document.getElementById('message')
+const resetBtn = document.getElementById('resetBtn')
 /*----------------------------- Event Listeners -----------------------------*/
 
 
