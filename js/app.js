fc874c96be14603de011f300b3b4322baaf2439c
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-04 22:31:00 -0500

    make social icons clickable and customize color

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@
 
 /*-------------------------------- Variables --------------------------------*/
 
+
 /*------------------------ Cached Element References ------------------------*/
+const player1nameInp = document.getElementById('player1-name')
+const player2nameInp = document.getElementById('player1-name')
+const squares = document.querySelectorAll('.blank')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
