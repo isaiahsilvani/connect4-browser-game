375f445d06ee15bae1f61bdb97a60d048864fbc7
isaiahsilvani <isaiahsilvani@outlook.com>
2021-03-05 13:49:52 -0500

    perfect grid system

--- js/app.js
+++ js/app.js
@@ -29,33 +29,50 @@ squares.forEach((square) => {
 /*-------------------------------- Functions --------------------------------*/
 function handleClick(evt) {
     let ele = evt.target
+    let position = ele.getAttribute('id')
     if (ele.className === 'blank') {
-    // IF THE SPACE CURRENT PLAYER CLICKS ON IS BLANK, set first number in  ID to 0 so we can start at the top. Then, CHECK THE BOTTOM SPACE IN BOARD TO SEE IF ITS EMPTY. IF IT IS, CHECK THE NEXT BOTTOM SPACE. WHEN NEXT BOTTOM IS FILLED, OR YOU HAVE REACHED THE BOTTOM, FILL BACKGROUND SPACE ACCORDING TO TURN (1 or -1)
-    ele.className = 'player1'
-        let position = ele.getAttribute('id')
-        // SET THE POSITION TO THE TOP, KEEP COLUMN THE SAME. SEPERATE ROW AND COLUMN IN ELE'S ID
         position = Number(position)
-        //get remainder of 7 to start position at the top of the board
         position = position % 7
         // if the top row of current column is null, increase position by 7. If new position on board is null, increasse by 7. If new position of board is null, increase by 7. If new position of board is filled with something other than null, go back 7 and change that element's class
-        let test = lastEmptySpaceInColumn(position)
-        console.log('output', test)
+        let emptySpace = document.getElementById(`${lastEmptySpaceInColumnV2(position)}`)
+        console.log(emptySpace)
+        //Now, set class of the emptySpace element to either player-2 or player-1, depending on what the turn is set to. Then, multiply turn by -1 to switch 
+        // if (turn === 1) {
+        //     emptySpace.removeAttribute('class')
+        //     emptySpace.setAttribute('class', )
+        // }
+
+    }
+}
+// For some reason, this recursive function is console.logging the expected result but is return undefined
+// function lastEmptySpaceInColumn(position) {
+//     console.log('position', position)
+//     let newPosition = position + 7;
+//     if (board[newPosition] === null) {
+//         lastEmptySpaceInColumn(newPosition)
+//     } else if (board[newPosition] !== null) {
+//         console.log('finalPosition', newPosition)
+//         //THIS IS WHERE THE PROBLEM IS!!
+//         console.log(newPosition - 7)
+//         console.log('for the love of god return test please')
+//         return "test"
+//     }
+// }
+
+
+
+function lastEmptySpaceInColumnV2(position) {
+    let newPosition = position + 7
+    for (let i = 0; i <= 7; i++) {
+        if (board[newPosition] === null) {
+            console.log('in-between indice: ', newPosition, ' boardValue: ', board[newPosition])
+            newPosition += 7
+    } else {
+        return newPosition - 7
+        }
     }
 }
 
-function lastEmptySpaceInColumn(position) {
-    console.log('position', position)
-    let newPosition = position + 7;
-    if (board[newPosition] === null) {
-        lastEmptySpaceInColumn(newPosition)
-    } else if (board[newPosition] !== null) {
-        console.log('finalPosition', newPosition)
-        //THIS IS WHERE THE PROBLEM IS!!
-        console.log(newPosition - 7)
-        console.log('for the love of god return poop please')
-        return "poop"
-    }
-}
 // return the number of the last empty space of a connect4 board, by checking to see if it's null. If we get a position >= 35, we have identifed that we are at the bottom of the board and it is time to break the loop and set the corresponding div id element with a player1 or player2 color
 function init() {
     //SET AN ARRAY TO REPRESENT THE GAME BOARD, PLAYER 1 = TURN WINNER IS NULL
